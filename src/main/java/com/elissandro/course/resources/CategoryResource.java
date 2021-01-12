9137280a0b2d015b144938c53a8f8241d13d173a
Elissandro <elissandroa@hotmail.com>
2021-01-12 18:26:44 -0300

    Category

---
+++ src/main/java/com/elissandro/course/resources/CategoryResource.java
@@ -0,0 +1,36 @@
+package com.elissandro.course.resources;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elissandro.course.entities.Category;
+import com.elissandro.course.entities.User;
+import com.elissandro.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findALL(){
+		List<Category> list = service.findALL();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/elissandro/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.elissandro.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elissandro.course.entities.Category;
+import com.elissandro.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findALL(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
