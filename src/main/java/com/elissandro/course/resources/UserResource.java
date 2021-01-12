b8f05ee2f7dce5f1e047f29d4102df5b9c9a38c1
Elissandro <elissandroa@hotmail.com>
2021-01-11 22:56:50 -0300

    H2 database, test profile, JPA

--- src/main/java/com/elissandro/course/resources/UserResource.java
+++ src/main/java/com/elissandro/course/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.elissandro.course.resources;
 
+import java.io.Serializable;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,9 @@ import com.elissandro.course.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResource {
-	
+public class UserResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@GetMapping
 	public ResponseEntity<User> findALL(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
